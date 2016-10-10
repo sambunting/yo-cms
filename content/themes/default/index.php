de1203e976e1ae1f9faa52be7ed5c282bd301fb4
Sam Bunting <sambunting2008@live.co.uk>
2016-10-10 22:01:24 +0100

    Updated theme stylesheet.

--- content/themes/default/index.php
+++ content/themes/default/index.php
@@ -1,3 +1,22 @@
-This is the theme homepage
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title><?php echo yo_get_blog_info("BLOG_NAME"); ?></title>
 
-<?php yo_get_posts() ?>
+		<link rel="stylesheet" type="text/css" href="content/themes/<?php echo yo_get_blog_info('THEME_NAME'); ?>/style/normalize.css">
+		<link rel="stylesheet" type="text/css" href="content/themes/<?php echo yo_get_blog_info('THEME_NAME'); ?>/style/stylesheet.css">
+	</head>
+	<body>
+		
+		<div id="main">
+			<h1><?php echo yo_get_blog_info("BLOG_NAME"); ?></h1>
+
+			<div id="post-listing">
+				<?php yo_get_posts() ?>
+			</div>
+		</div>
+
+		
+	</body>
+</html>
